7d0bae441c383e803c24c4836e25aa4cc7bb8bd9
chino-m <qlabsph@gmail.com>
2024-06-13 03:33:09 +0800

    Minor Update
    Updated Address

--- frontend/src/address.js
+++ frontend/src/address.js
@@ -1,6 +1,6 @@
-const local = 'http://127.0.0.1:8000';
-// const host = 'https://aquaeasy.onrender.com';
+// const local = 'http://127.0.0.1:8000';
+const host = 'https://aquaeasy.onrender.com';
 
-const current_address = local;
+const current_address = host;
 
 export default current_address;
